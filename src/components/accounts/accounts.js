ffd804f5a09fa8821771fee7df3f9058bca9cec8
Daniel Frey <dann.frey@me.com>
2018-11-14 13:57:22 -0800

    added links

--- src/components/accounts/accounts.js
+++ src/components/accounts/accounts.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 // actions
 import * as dataActions from '../../action/data';
 import PropTypes from "prop-types";
+import { Link } from 'react-router-dom';
 
 class Accounts extends React.Component {
   constructor(props) {
@@ -38,6 +39,7 @@ class Accounts extends React.Component {
           <button onClick={this.handleShowList}>Gen User List</button>
           <p>This will be the Accounts page.</p>
           {this.state.showList === true ? this.genUserList() : null}
+          <Link to='/dashboard' className='centered'>Dashboard</Link>
         </div>
     );
   }

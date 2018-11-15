b075ffc6e5cebd419b1597e1f9f76305c08a17ab
Tom <tnorth93@gmail.com>
2018-11-15 11:41:16 -0800

    created part reducer

--- src/reducer/main-reducer.js
+++ src/reducer/main-reducer.js
@@ -2,5 +2,6 @@ import { combineReducers } from 'redux';
 import token from './token-reducer';
 import users from './users-reducer';
 import subAssy from './sub-assy-reducer';
+import part from './part-reducer';
 
-export default combineReducers({ token, users, subAssy });
+export default combineReducers({ token, users, subAssy, part });

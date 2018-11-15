23c5b0fd581c0d71fda91ce6748d480d9c5cb188
Daniel Frey <dann.frey@me.com>
2018-11-14 22:06:41 -0800

    it works

--- src/components/data-table/data-utils.js
+++ src/components/data-table/data-utils.js
@@ -13,16 +13,26 @@ const range = len => {
 
 const newPart = () => {
   return {
-    subId: faker.random.number(100000),
-    subPart: faker.random.number(4444),
-    subVersion: faker.random.number(4444),
-    subQuantity: faker.random.number(4444),
-    subMinutes: faker.random.number(4444),
-    partIds: faker.random.number(4444),
+    subId: '2',
+    subPart: 'password',
+    subVersion: 'question',
+    subQuantity: 'answer',
+    subMinutes: 'ok',
+    partIds: [
+      '5bece67b90c80d89d79c84f5',
+      '5bece68490c80d89d79c84f6',
+      '5bece68e90c80d89d79c84f7',
+    ],
+    // subId: faker.random.number(100000),
+    // subPart: faker.random.number(4444),
+    // subVersion: faker.random.number(4444),
+    // subQuantity: faker.random.number(4444),
+    // subMinutes: faker.random.number(4444),
+    // partIds: faker.random.number(4444),
   };
 };
 
-export function makeData(len = 5553) {
+export function makeData(len = 1) {
   return range(len).map(d => {
     return {
       ...newPart(),

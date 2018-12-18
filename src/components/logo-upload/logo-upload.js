688799d9cce0742580e17151f2e45fd9a8677768
Benjamin G. West <23172276+bgwest@users.noreply.github.com>
2018-12-17 21:55:03 -0800

    updates to logo upload form and POST to back-end

--- src/components/logo-upload/logo-upload.js
+++ src/components/logo-upload/logo-upload.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import LogoUploadForm from '../logo-upload-form/logo-upload-form';
 import NavUi from '../nav-ui/nav-ui';
+import superagent from 'superagent';
+import * as routes from '../../routes';
+import { set } from '../../action/data';
 
 // import * as routes from '../../routes';
 
@@ -12,11 +15,28 @@ class LogoUpload extends React.Component {
     this.state = {};
   }
 
+  handleUpload(data) {
+    console.log('got data');
+    console.log('data.selectedFile');
+    console.log(data.selectedFile);
+    console.log('attempting upload');
+    superagent.post(`http://localhost:3000${routes.POST_LOGO_BACKEND}`)
+      .attach('image', data.selectedFile)
+      .then((response) => {
+        let getResponse = response;
+        getResponse = JSON.parse(response.text);
+        console.log('getResponse');
+        console.log(getResponse);
+      }).catch(console.error);
+  }
+
   render() {
     return (
       <section>
         <NavUi location={this.props.location}/>
-        <LogoUploadForm type="upload"/>
+        <br/>
+        <br/>
+        <LogoUploadForm onComplete={this.handleUpload.bind(this)} type="upload"/>
       </section>
     );
   }

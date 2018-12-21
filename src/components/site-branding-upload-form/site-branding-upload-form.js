1f0455d427addeb26e43a8daa1957f2bec0f4646
Benjamin G. West <23172276+bgwest@users.noreply.github.com>
2018-12-20 16:14:29 -0800

    re-worked draggable upload - now working but needs redux management

--- src/components/site-branding-upload-form/site-branding-upload-form.js
+++ src/components/site-branding-upload-form/site-branding-upload-form.js
@@ -9,13 +9,16 @@ class SiteBrandingUploadForm extends React.Component {
     this.emptyState = {
       selectedFile: [],
       loaded: 0,
+      classNames: 'dragHere',
+      uploadComplete: false,
     };
     this.state = this.emptyState;
   }
 
   handleFile(event) {
+    const files = event.target ? event.target.files[0] : event[0];
     this.setState({
-      selectedFile: event.target.files[0],
+      selectedFile: files,
       loaded: 0,
     });
   }
@@ -23,7 +26,7 @@ class SiteBrandingUploadForm extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
     this.props.onComplete(this.state);
-    this.setState(this.emptyState);
+    this.setState({ ...this.emptyState, uploadComplete: true });
   }
 
   renderUploadButton() {
@@ -32,6 +35,47 @@ class SiteBrandingUploadForm extends React.Component {
 
   renderUploadPlaceHolder() {
     return <p className="uploadPlaceHolder">Upload Logo</p>;
+  }
+
+  dragDrop(event) {
+    event.preventDefault();
+    console.log('dragDrop ran');
+    let data;
+    // data = event.dataTransfer.getData('image/png');
+    data = event.dataTransfer.files;
+
+    // Do something with the data
+    console.log(data);
+    this.setState({ classNames: 'dragHere' });
+    this.handleFile(data);
+  }
+
+  dragStart(event) {
+    event.preventDefault();
+    console.log('dragStart ran');
+  }
+
+  dragOver(event) {
+    event.preventDefault();
+    this.setState({ classNames: 'dragHere imageDrop' });
+  }
+
+  dragExit(event) {
+    event.preventDefault();
+    this.setState({ classNames: 'dragHere' });
+  }
+
+  fileIsLoaded(file) {
+    return <h2><span className='loadedBaby'>{file}</span> loaded.</h2>;
+  }
+
+  dropText(type) {
+    if (type === 'after') {
+      return <span>drop :)</span>;
+    }
+    if (type === 'before') {
+      return <span>&hellip; or drag here and &hellip;</span>;
+    }
   }
 
   render() {
@@ -46,21 +90,28 @@ class SiteBrandingUploadForm extends React.Component {
               action=""
               encType="multipart/form-data">
           <div className="box__input">
-            <input className="box__file"
-                   onChange={this.handleFile.bind(this)}
-                   type="file"
-                   name="files[]"
-                   id="file"
-                   data-multiple-caption="{count} files selected" multiple/>
-            <section className="dragHere">
+            {!this.state.selectedFile.name
+              ? <input className="box__file"
+                       onChange={this.handleFile.bind(this)}
+                       type="file"
+                       name="files[]"
+                       id="file"
+                       data-multiple-caption="{count} files selected" multiple/>
+              : this.fileIsLoaded(this.state.selectedFile.name) }
+            {!this.state.selectedFile.name
+              ? <section className={this.state.classNames} draggable="true"
+                         onDragOver={this.dragOver.bind(this)} onDragStart={this.dragStart}
+                         onDrop={this.dragDrop.bind(this)} onDragExit={this.dragExit.bind(this)}>
             <label htmlFor="file">
-              <span className="box__dragndrop">&hellip; or drag it here :)</span>
+              <span className="box__dragndrop">{this.state.classNames === 'dragHere imageDrop' ? this.dropText('after') : this.dropText('before')}</span>
             </label>
             </section>
+              : null}
             <p><i>notes:</i></p>
             <p><i>- the image must be named: company-logo</i></p>
             <p><i>- accepted extensions: .jpg, .jpeg, .png</i></p>
             {this.state.selectedFile.name ? this.renderUploadButton() : this.renderUploadPlaceHolder()}
+            {this.state.uploadComplete ? <h1 className='uploadComplete'>Upload complete.</h1> : null}
           </div>
         </form>
       </div>

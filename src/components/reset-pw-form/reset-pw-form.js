f03148a08c9677203f1644939b02c55fa1cbccc5
Benjamin G. West <23172276+bgwest@users.noreply.github.com>
2019-02-19 08:38:59 -0800

    added conditional rendering for reset vs forgot

--- src/components/reset-pw-form/reset-pw-form.js
+++ src/components/reset-pw-form/reset-pw-form.js
@@ -23,43 +23,53 @@ class ResetPwForm extends React.Component {
     console.log(event.target);
   };
 
+  resetPwForm = () => {
+    return <form className="auth-form" onSubmit={this.handleSubmit}>
+        <li>
+          <label>Current Password</label>
+          <input
+            name="currentPassword"
+            placeholder="Current Passsword"
+            type="password"
+            value={this.state.currentPassword}
+            onChange={this.handleChange}
+          />
+        </li>
+        <li>
+          <label>New Password</label>
+          <input
+            name="newPassword"
+            placeholder="New Passsword"
+            type="password"
+            value={this.state.newPassword}
+            onChange={this.handleChange}
+          />
+        </li>
+        <li>
+          <label>Verify New Password</label>
+          <input
+            name="verifyNewPassword"
+            placeholder="Verify New Password"
+            type="password"
+            value={this.state.verifyNewPassword}
+            onChange={this.handleChange}
+          />
+        </li>
+        <button type="submit">submit</button>
+      </form>;
+  };
+
+  forgotPwForm = () => {
+   return <p>forgot my pw</p>;
+  };
+
   render() {
+    const { type } = this.props;
     return (
-     <div className="createForm">
-       <form className="auth-form" onSubmit={this.handleSubmit}>
-         <li>
-           <label>Current Password</label>
-           <input
-             name="currentPassword"
-             placeholder="Current Passsword"
-             type="password"
-             value={this.state.currentPassword}
-             onChange={this.handleChange}
-           />
-         </li>
-           <li>
-           <label>New Password</label>
-           <input
-             name="newPassword"
-             placeholder="New Passsword"
-             type="password"
-             value={this.state.newPassword}
-             onChange={this.handleChange}
-           />
-           </li>
-           <li>
-           <label>Verify New Password</label>
-           <input
-             name="verifyNewPassword"
-             placeholder="Verify New Password"
-             type="password"
-             value={this.state.verifyNewPassword}
-             onChange={this.handleChange}
-           />
-         </li>
-         <button type="submit">submit</button>
-       </form>
-     </div>
+      <div className="createForm">
+        { type === 'reset' ? this.resetPwForm() : null }
+        { type === 'forgot' ? this.forgotPwForm() : null }
+      </div>
     );
   }
 }

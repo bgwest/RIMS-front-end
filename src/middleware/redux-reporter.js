868e228fb9737cbd318e082970e359a4d7da3da1
Benjamin G. West <23172276+bgwest@users.noreply.github.com>
2019-03-12 23:16:12 -0700

    added additonal debug log to redux-reporter while this piece is still in development

--- src/middleware/redux-reporter.js
+++ src/middleware/redux-reporter.js
@@ -3,6 +3,7 @@ export default store => next => (action) => {
     console.log('__ACTION__', action);
     const result = next(action);
     console.log('__NEW STATE__', store.getState());
+    console.log('__VIEW STORE OBJ__', store);
     return result;
   } catch (error) {
     console.log('__ERROR__', error);
